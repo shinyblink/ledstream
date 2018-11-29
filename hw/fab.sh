9c13f431f2a657421c538fb549e359fd74c060e5
Adrian Pistol <vifino@tty.sh>
2018-11-29 05:52:20 +0100

    hw/bx-hub75: reroute to 0.1m minimum trace spacing

--- hw/fab.sh
+++ hw/fab.sh
@@ -9,12 +9,16 @@ fi
 FABFILE="$1"
 OUTFILE="$(readlink -f "$2")"
 PCBFILE="$3"
-NAME="$(basename "$PCBFILE" | sed 's/\.kicad_pcb//')"
+NAME="$(basename "$OUTFILE" | sed 's/\.zip//')"
 
 TMP="$(mktemp -d)"
 mkdir -p "$TMP/$NAME"
 
+echo "[*] Generating Gerbers..."
 kifab -o "$TMP/$NAME" "$PCBFILE" "$FABFILE"
+
+echo "[*] Zipping up.."
 cd $TMP
 zip -r "$OUTFILE" "$NAME/"
 rm -r "$TMP"
+echo "[*] Done."
